cf33c73dad39986d50acc4d7c629d1938a80685c
IT21216046 <it21216046@my.sliit.lk>
2023-09-03 01:00:49 +0530

    "pushing users"

---
+++ backend/routes/adminRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const{
+    getAdmin,
+    createAdmin,
+    getaAdmin,
+    loginAdmin,
+
+} = require('../controllers/adminController')
+
+//const requireAuth = require('../middleware/requireAdminAuth')
+
+const router = express.Router()
+
+//router.use(requireAuth)
+
+router.get('/', getAdmin)
+
+router.post('/', createAdmin)
+
+router.post('/login', loginAdmin)
+
+router.get('/:id', getaAdmin)
+
+module.exports = router
