dfe8ac1fe197a181af6ef5c71cbcf107e1ee19e2
IT21312380 <it21312380@my.sliit.lk>
2023-09-04 20:19:39 +0530

    login ui

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,48 +1,55 @@
-import { useState } from "react";
-import { useLogin } from '../hooks/useLogin'
-import { useAuthContext } from '../hooks/useAuthContext'
+import React, { useState } from "react";
+import { useLogin } from '../hooks/useLogin';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { useNavigate } from "react-router-dom"; // Import useNavigate
+import './login.css';
 
+const UserLogin = () => {
+  const { user } = useAuthContext;
+  const [username, setUser] = useState('');
+  const [pw, setPW] = useState('');
+  const { login, error, isLoading } = useLogin();
+  const navigate = useNavigate(); // Use useNavigate hook
 
-const UserLogin = () =>{
-    const { user } = useAuthContext
-    const [username,setUser] = useState('')
-    const [pw,setPW] =useState('')
-    const {login, error, isLoading} = useLogin()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, pw);
 
-    const handleSubmit = async(e) =>{
-        e.preventDefault()
-
-        await login(username,pw)
-
+    if (user) {
+     
+      navigate("/cart");
     }
+  }
 
-    return(
-        <form className="login" onSubmit={handleSubmit}>
-            
-
-            <label>Username:</label>
-            <input
-                type="text"
-                onChange={(e) => setUser(e.target.value)}
-                value={username}  
-            />
-            <label>Password:</label>
-            <input
-                type="password"
-                onChange={(e) => setPW(e.target.value)}
-                value={pw}  
-            />
-            <br/>
-
-            <button className="btnSubmit" disabled = {isLoading}>Login</button>
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            onChange={(e) => setUser(e.target.value)}
+            value={username}
+            placeholder="Enter your username"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPW(e.target.value)}
+            value={pw}
+            placeholder="Enter your password"
+          />
+        </div>
+        <button className="btn-submit" disabled={isLoading}>Login</button>
         {error && <div className="error">{error}</div>}
-
-
-        </form>
-    )
-
-
-
+      </form>
+    </div>
+  );
 }
 
-export default UserLogin
+export default UserLogin;
